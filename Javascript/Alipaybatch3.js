7ee0efe0b350068fcaea323e339559301d2f514c
T0sng <123780955+T0sng@users.noreply.github.com>
2023-12-28 02:17:15 +0800

    Update Alipaybatch3.js

--- Javascript/Alipaybatch3.js
+++ Javascript/Alipaybatch3.js
@@ -1,4 +1,6 @@
-var headers = $response.headers;
-let obj = JSON.parse($response.body['resultObj'] );
-headers['Content-Type'] = 'application/json;charset=UTF-8';
-
+let headers = $request.headers;
+headers['user-agent'] = 'Mozilla/5.0 (iPhone; CPU iPhone OS 17_1_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/21B101 ChannelId(102) Ariver/1.1.0 AliApp(AP/10.5.56.6000) Nebula WK RVKType(1) AlipayDefined(nt:WIFI,ws:430|868|3.0) AlipayClient/10.5.56.6000 Language/en Region/CN NebulaX/1.0.0';
+delete headers['sec-ch-ua-platform'];
+delete headers['sec-ch-ua'];
+delete headers['sec-ch-ua-mobile'];
+$done({headers});
