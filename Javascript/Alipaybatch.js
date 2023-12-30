4105c81324e581f0121c11a521c19f9909e1d7f6
T0sng <123780955+T0sng@users.noreply.github.com>
2023-12-30 15:04:20 +0800

    Update Alipaybatch.js

--- Javascript/Alipaybatch.js
+++ Javascript/Alipaybatch.js
@@ -14,27 +14,8 @@ var obj = {
             {
                 "subBizSceneDesc": "妈的，跟美国，俄国，意大利，英国，德国，法国，蒙古，智利，巴西，韩国，朝鲜，日本，菲律宾，印尼，印度，巴基斯坦，泰国，加拿大，墨西哥，南非拼了!",
                 "subBizScene": "OTHER"
-            },
-            {
-                "subBizSceneDesc": "和日本拼了",
-                "subBizScene": "COMMISSION"
-            },
-            {
-                "subBizSceneDesc": "和大英帝国拼了",
-                "subBizScene": "REFUND"
-            },
-            {
-                "subBizSceneDesc": "和以色列拼了",
-                "subBizScene": "EXPENSE"
-            },
-            {
-                "subBizSceneDesc": "和泰国拼了",
-                "subBizScene": "FREIGHT"
-            },
-            {
-                "subBizScene": "OTHER",
-                "subBizSceneDesc": "和澳大利亚拼了"
             }
+
         ]
     }
 };
